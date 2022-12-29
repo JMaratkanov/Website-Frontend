4236765a388058e36bfcedb71a07f6d432ddf529
Johann M <j.maratkanov@outlook.de>
2022-12-29 14:16:20 +0100

    Bezug von Daten aus der Db ins FE implementiert

--- src/views/SecondPage.vue
+++ src/views/SecondPage.vue
@@ -1,25 +1,43 @@
 <template>
   <div>
-    <h1 style="text-align: center;">Über uns</h1>
-   <button @click="zurueck" class="btn btn-light">Zurück</button>
+    <h1 style="text-align: center">Über uns</h1>
+    <p style="margin-left: 150px">{{ notification }}</p>
+
+    <button @click="zurueck" class="btn btn-light">Zurück</button>
     <button @click="weiter" class="btn btn-secondary">Weiter</button>
   </div>
 </template>
 
-<script>import { mapGetters, mapActions } from "vuex";
+<script>
+import { mapGetters, mapActions } from "vuex";
 
 export default {
-
   name: "SecondPage",
+  data: () => {
+    return {
+      notification: "Lädt",
+    };
+  },
+  created() {
+    const baseUrl = import.meta.env.VITE_BASE_URL;
 
+    this.axios
+      .get(baseUrl + "/notification/ewr3jd")
+      .then((response) => {
+        this.notification = response.data;
+      })
+      .catch((error) => {
+        console.error("Der Request ist gescheitert", error);
+      });
+  },
   methods: {
     ...mapActions(["setCurrentPage"]),
-    zurueck(){
+    zurueck() {
       this.setCurrentPage(1);
     },
-    weiter(){
+    weiter() {
       this.setCurrentPage(3);
-    }
+    },
   },
 };
 </script>
